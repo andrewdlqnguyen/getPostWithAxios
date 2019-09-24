0af8148cf557d976bc59718dfa51833875b1c5b8
Unknown <andrewdlqnguyen@gmail.com>
2019-09-24 15:14:12 -0400

    Redirect Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import {Route, NavLink, Switch } from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from '././Posts/Posts';
-import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
@@ -15,13 +14,13 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                                to="/" 
+                                to="/posts/" 
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
-                                }}>Home</NavLink></li>
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: "#submit",
@@ -32,10 +31,10 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts}/>
                 <Switch>
                     <Route path="/new-post" component={NewPost}/>
-                    <Route path="/:id" exact component={FullPost}/>
+                    <Route path="/posts" component={Posts}/>     
+                    <Redirect from="/" to="/posts"/>
                 </Switch>               
                 </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Route } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 //import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
@@ -35,7 +37,7 @@ class Posts extends Component {
     // }
 
     postSelectedHandler = (id) => {
-        this.props.history.push({pathname: '/' + id});
+        this.props.history.push({pathname: '/posts/' + id});
     }
 
     render() {
@@ -55,9 +57,12 @@ class Posts extends Component {
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + "/:id"} exact component={FullPost}/>
+            </div>
         );
     }
 }
